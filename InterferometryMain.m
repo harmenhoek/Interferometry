9791a0a3629da1e0c99583af85cc7ed45018d972
harmenhoek <62509463+harmenhoek@users.noreply.github.com>
2022-01-25 16:45:12 +0100

    Plotting organized in Plot.m
    Plot.m is a (static) class with plotting functions.

--- InterferometryMain.m
+++ InterferometryMain.m
@@ -42,6 +42,8 @@ Settings.Analyze_TwoParts_CutOff = 1473;
 
 %% SETTINGS
 
+Settings.AnalyzeFolder = false;
+
 Settings.Lambda = 520e-9;                        % Wavelength of light in meters.
 
 Settings.NumberSlices = 400;                     % Total number of radial slices in the full 2pi
@@ -60,7 +62,15 @@ Settings.PlotSingleSlice = 4;                    % Show several analysis steps o
 Settings.ShowHeightProfileProgress = true;       % Show progress of Height Profile calculation of all slices.
 
 % Plotting
-Settings.Plot_Average = true;                    % Calculate average multiple slices (consider analyzing only a quadrant).
+Settings.Show_Plots = true;
+    Settings.Plot_VisualizeSlices = true;
+    Settings.Plot_SingleSlice = true;
+    Settings.Plot_Surface = false; % not working properly
+    Settings.Plot_Contour = true;
+        Settings.Plot_Contour_OverlayOnImage = true;
+        Settings.Plot_Contour_Levels = 10;
+        Settings.Plot_Contour_Transparency = 0.6;
+    Settings.Plot_AverageHeight = true;                    % Calculate average multiple slices (consider analyzing only a quadrant).
 
 % Saving
 Settings.Save_Figures = false;
@@ -114,7 +124,7 @@ if ~any(strcmp({'.tif', '.tiff', '.png', '.jpg', '.jpeg', '.bmp', '.gif'}, ext))
 end
 
 status = CheckIfClass('numeric', {'Settings.Lambda', 'Settings.PlotSingleSlice', 'Settings.NumberSlices', 'Settings.SectorStart', 'Settings.SectorEnd'});
-status2 = CheckIfClass('logical', {'Settings.AnalyzeSector', 'Settings.ShowHeightProfileProgress', 'Settings.FilterBy_AmountExtrema', 'Settings.Plot_Average', 'Settings.Save_Figures', 'Settings.Save_PNG', 'Settings.Save_TIFF', 'Settings.Save_FIG', 'Settings.ShowLogoAtStart'});
+status2 = CheckIfClass('logical', {'Settings.AnalyzeSector', 'Settings.ShowHeightProfileProgress', 'Settings.FilterBy_AmountExtrema', 'Settings.Plot_AverageHeight', 'Settings.Save_Figures', 'Settings.Save_PNG', 'Settings.Save_TIFF', 'Settings.Save_FIG', 'Settings.ShowLogoAtStart'});
 status3 = CheckIfClass('char', {'Settings.Save_Folder'});
 if min([status, status2, status3]) == 0
     Logging(1, 'Could not continue because of invalid settings (see WARNINGs above).')
@@ -196,6 +206,8 @@ Settings.PeakFitSettings.b.MinPeakDistance = Settings.MinPeakDistance_outside;
 Settings.PeakFitSettings.a.Smoothing = Settings.Smoothing_inside;
 Settings.PeakFitSettings.b.Smoothing = Settings.Smoothing_outside;
 
+% Check wether images > N, then Show_Plots = False
+
 
 %%
 clear ext steps maxres minres status msg path name extensions savefolder_sub
@@ -268,60 +280,13 @@ end
 
 % Select TwoPart CutOff point
 if Settings.Analyze_TwoParts && ~isfield(Settings, 'Analyze_TwoParts_CutOff')
-    %     Logging(2, 'No image center given in settings, pick image center now.')
-    if exist('f1')
-        figure(1)
-        clf(f1)
-    else
-        f1 = figure(1);
-    end
-    imshow(I)
-    hold on
-    for k = 1:length(theta_all)
-        pnt = points(k, :);
-        roi = [pnt; Settings.Interferometry_Center];
-        plot(roi(:,1), roi(:,2), 'Color', 'red', 'LineWidth', 2)
-        plot(pnt(:,1), pnt(:,2), '.', 'Color', 'red' , 'MarkerSize', 20)
-        text(pnt(:,1), pnt(:,2), num2str(k))
-        clear pnt roi
-    end
-    pnt = drawpoint;
-    Settings.Analyze_TwoParts_CutOff = norm(pnt.Position-Settings.Interferometry_Center);
-    close(f1)
+    Settings.Analyze_TwoParts_CutOff = Plot.VisualizeSlicesCutOff(Settings, struct('I',I,  'theta_all',theta_all, 'points',points));
 end
 Settings.PeakFitSettings.CutOffValue = Settings.Analyze_TwoParts_CutOff;
 
-
-% Show slices
-
-show_slices = true;
-
-if show_slices   
-    if exist('f1')
-        figure(1)
-        clf(f1)
-    else
-        f1 = figure(1);
-    end
-    imshow(I)
-    hold on
-    for k = 1:length(theta_all)
-        pnt = points(k, :);
-        roi = [pnt; Settings.Interferometry_Center];
-        plot(roi(:,1), roi(:,2), 'Color', 'red', 'LineWidth', 2)
-        plot(pnt(:,1), pnt(:,2), '.', 'Color', 'red' , 'MarkerSize', 20)
-        text(pnt(:,1), pnt(:,2), num2str(k))
-        clear pnt roi
-    end
-    if Settings.Analyze_TwoParts
-        if Settings.AnalyzeSector
-            plot_arc(Settings.SectorStart, Settings.SectorEnd, Settings.Interferometry_Center(1), Settings.Interferometry_Center(2), Settings.Analyze_TwoParts_CutOff)
-        else 
-            plot_arc(0, 2*pi, Settings.Interferometry_Center(1), Settings.Interferometry_Center(2), Settings.Analyze_TwoParts_CutOff)
-        end
-    end
-    SaveFigure(Settings.Save_Figures, f1, save_extensions, append(basename, '_SlicesOverview'));
-end % if show_slices
+% Visualize slices
+f1 = Plot.VisualizeSlices(Settings, struct('I',I,  'theta_all',theta_all, 'points',points));
+SaveFigure(min([Settings.Save_Figures Settings.Plot_VisualizeSlices]), f1, save_extensions, append(basename, '_SlicesOverview'));
 
 clear f1 f2 k dtheta savename show_slices
 Logging(6, 'Slices determined successfully.')
@@ -337,7 +302,6 @@ slice_lengths = nan(length(theta_all), 1);
 no_height_profile = 0;
 first_empty_row = 1;
 for k = 1:length(points)  % iterate over all the end points (same length as all slices to analyze)
-% for k=20
     if mod(k, round(length(theta_all)/10)) == 0 && Settings.ShowHeightProfileProgress
         Logging(5, append("Height Profile calculation of all slices at ", num2str(k/round(length(theta_all)/10)*10), '%.'))
     end
@@ -358,52 +322,15 @@ for k = 1:length(points)  % iterate over all the end points (same length as all 
     end
 
     
-
-    
     % Plot one slice
     if k == Settings.PlotSingleSlice
-        if exist('f4')
-            figure(4)
-            clf(f4)
-        else
-            f4 = figure(4);
-        end
-        
-        tiledlayout(3,1,'TileSpacing','tight');
-        
-        c_l = 1:length(c_or);
-        nexttile;
-        plot(c_l, c_or, 'Color', 'red', 'LineWidth', 2)
-        xlabel('Distance from center [pix]')
-        ylabel('Intensity [arb. units]')
-        xlim([0, max(c_l)])
-        title(strcat('Original slice k=', num2str(k)))
-        
-        nexttile;
-        line(c_l, c_nor, 'Color', 'red', 'LineWidth', 2)
-        hold on
-        plot(c_l(pks_locs), pks, '.', 'MarkerSize', 10, 'Color', 'black')
-        plot(c_l(mns_locs), -mns, '.', 'MarkerSize', 10, 'Color', 'green')
-        xlabel('Distance from center [pix]')
-        ylabel('Intensity [arb. units]')
-        xlim([0, max(c_l)])
-        title('After img adjustments and normalization')
-        
-        nexttile;
-%         plot((c_l(end)-length(d_final)+1):c_l(end), d_final.*1e6, 'Color', 'blue', 'LineWidth', 2)
-        plot(c_l, d_final.*1e6, 'Color', 'blue', 'LineWidth', 2)
-        xlabel('Distance from center [pix]')
-        ylabel('Height [um]')
-        xlim([0, max(c_l)])
-        title('Height profile after model fit')
-
-
-        SaveFigure(Settings.Save_Figures, f4, save_extensions, append(basename, '_Slice', num2str(k)));
-    end % plotline
+        f4 = Plot.SingleSliceAnalysis(Settings, struct('c_or',c_or, 'c_nor',c_nor,  'pks',pks, 'pks_locs',pks_locs, 'mns',mns, 'mns_locs',mns_locs, 'd_final',d_final, 'slicenumber',k));
+        SaveFigure(min([Settings.Save_Figures Settings.Plot_SingleSlice]), f4, save_extensions, append(basename, '_Slice', num2str(k)));
+    end
     
     clear pnt roi xx yy c_l c_nor c_or mns_locs pks_locs pks mns d_final
 
-end % points
+end % iteration over all slices
 
 if no_height_profile == length(points)
     Logging(1, 'No height profiles could be calculated for any of the slices. Check if slices are not to short')
@@ -435,183 +362,27 @@ if Settings.FilterBy_AmountExtrema
     clear k MedianExtrema TotalExtrema cntr
 end
 
-
-
 %Convert to single array and Remove NaNs
 data_all = vertcat(data_cell{:});
 data_all(any(isnan(data_all), 2), :) = []; % remove nan values
 
 Logging(6, 'Filtering and postprocessing completed successfully.')
 
-%% 5 - Create surface and plot
+%% 5 - Plotting of results
 
-Logging(5, '-- 5/6 -- Plotting started.')
+Logging(5, '-- 5/6 -- Plotting of results started.')
 
-Plot_Surface = false;
-if Plot_Surface
-    [newXY,~,locMembers] = unique([data_all(:,1), data_all(:,2)], 'rows', 'stable');  % if slices overlap, filter out doubles
-    xx = newXY(:,1)';
-    yy = newXY(:,2)';
-    zz = splitapply(@(v) mean(v), data_all(:,3)', locMembers');  % group the values of z by the duplicate pairs, take the mean
+f5 = Plot.Surface(Settings, struct('data_all',data_all));
+SaveFigure(min([Settings.Save_Figures Settings.Plot_Surface]), f5, save_extensions, append(basename, '_Surface'));
 
-    T = delaunay(xx, yy);
-    if exist('f5')
-        figure(5)
-        clf(f5)
-    else
-        f5 = figure(5);
-    end
+f7 = Plot.Contour(Settings, struct('I_or',I_or, 'data_all',data_all));
+SaveFigure(min([Settings.Save_Figures Settings.Plot_Contour]), f7, save_extensions, append(basename, '_Contour'));
 
-    % tri surface
-         trisurf(T, yy, xx, -zz+max(zz), 'EdgeColor', 'none')
-
-    % 3d scatter plot
-        % scatter3(data_all(:,1), data_all(:,2), data_all(:,3), [], data_all(:,3), 'filled')
-
-    % contour plot (custom function)
-        % x = data_all(:,1);
-        % y = data_all(:,2);
-        % z = data_all(:,3);
-        % tricontour(T, x, y, z, linspace(0, max(z), 100))
-        % 
-
-    % xlim([0, I_size(1)])
-    % ylim([0, I_size(2)])
-    
-    clear f5 newXY locMembers
-
-    end
-
-Plot_Contour = true;
-if Plot_Contour
-    if exist('f7')
-        figure(7)
-        clf(f7)
-    else
-        f7 = figure(7);
-    end
-    
-    OverlayContourOnImage = true;
-    if OverlayContourOnImage
-        
-        imshow(I_or)
-        hold on
-        
-        
-    end
-    
-    
-    % tri surface
-    x = data_all(:,1);
-    y = data_all(:,2);
-    z = data_all(:,3);
-    [xq,yq] = meshgrid(linspace(min(x),max(x),sqrt(length(x))), ...
-        linspace(min(y),max(y),sqrt(length(y))));
-    warning('off', 'all')  % TODO: remove duplicate x,y datapoints (average z). Now this throws a warning by griddate (and it is done automatically).
-    vq = griddata(x,y,z,xq,yq);
-    warning('on', 'all')
-%     s = surf(yq,xq,-vq+max(max(vq)),'EdgeColor','none', 'FaceLighting','gouraud');
-    linetype = '-';  % '-' ':' ':' '-,'  or add colors: '--g'
-    levels = 10;
-    [~, hContour] = contourf(xq,yq,vq, levels, linetype);
-    hContour.LineWidth = .2;
-    hContour.LineColor = [.5 .5 .5];
-    
-    
-    if OverlayContourOnImage
-        drawnow;
-        hFills = hContour.FacePrims;  % array of TriangleStrip objects
-        [hFills.ColorType] = deal('truecoloralpha');  % default = 'truecolor'
-        Transparency = 0.6;
-        for idx = 1 : numel(hFills)
-            hFills(idx).ColorData(4) = Transparency * 255;   % default=255
-        end
-    end
-    set(gca, 'YDir','reverse')
-    ylim([0, I_size(1)])
-    xlim([0, I_size(2)])
-    colorbar;
-    
-    
-    
-    SaveFigure(Settings.Save_Figures, f7, save_extensions, append(basename, '_Contour'));
-    clear f7 OverlayContourOnImage
-end
-
-clear Plot_Surface Transparency vq x xq y yq z data_all hContour hFills idx linetype levels Plot_Contour
-
-
-
-
-
-%%
-
-% Plot average slice   TODO FIX after reversing direction not working
-% anymore
-if Settings.Plot_Average
-    if exist('f6')
-        figure(6)
-        clf(f6)
-    else
-        f6 = figure(6);
-    end
-    data_cell_noempties = data_cell(cellfun(@(x) ~isempty(x), data_cell));
-    A = cellfun(@(x) x(:,3), data_cell_noempties, 'UniformOutput', false);
-    array_sizes = cellfun(@(x) length(x(find(~isnan(x),1):end)), A); %exclude leading nans
-    shortest_array = min(array_sizes(array_sizes ~= 0));
-
-    B = nan(length(A), shortest_array);
-    for i = 1:length(A)
-        if max(~isnan(A{i}))
-            first_nonnan = find(~isnan(A{i}),1);
-            data = A{i}(first_nonnan:end);
-            B(i, :) = data(end-shortest_array+1:end);
-        end
-    end
-    mean_array = mean(B, 'omitnan'); %change to this line
-
-
-    %     for i = 1:length(A)
-    %         first_nonnan = find(~isnan(A{i}),1);
-    %         A{i} = A{i}(first_nonnan:end);
-    % 
-    %     end
-
-
-        % REDO INTERPOLATE. JUST IGNORE OUTER DATA! AND SHOW IN NEW FIG.
-    %     average_slice = CellMeanInterpolate(A');
-    %     average_slice = CellMean(A', 'back');
-
-        plot(mean_array, 'LineWidth', 3)
-        xlabel('Distance from center[pix]')
-        ylabel('Height [um]')
-    %     xlim([0, length(average_slice)]) 
-    %     clear A
-    % test changes here
-
-    SaveFigure(Settings.Save_Figures, f6, save_extensions, append(basename, '_AverageSlice'));
-    clear f6 first_nonnan i A B array_sizes 
-end
-
-
-
-Plot_Next = false;
-if Plot_Next
-    if exist('f8')
-        figure(8)
-        clf(f8)
-    else
-        f8 = figure(8);
-    end
-    
-    
-    
-end
-
+f6 = Plot.AverageHeight(Settings, struct('data_cell',data_cell));
+SaveFigure(min([Settings.Save_Figures Settings.Plot_AverageHeight]), f6, save_extensions, append(basename, '_AverageSlice'));
 
 Logging(6, 'Plotting finished successfully.')
 
-clear Slice_Endpoints shortest_array Plot_Next data
 %% 6 - Save data
 
 Logging(5, '-- 6/6 -- Saving data started.')
@@ -649,18 +420,5 @@ function SaveFigure(saving_on, fig, extensions, name)
     end
 end
 
-function P = plot_arc(a,b,h,k,r)
-    % Plot a circular arc as a pie wedge.
-    % a is start of arc in radians, 
-    % b is end of arc in radians, 
-    % (h,k) is the center of the circle.
-    % r is the radius.
-    t = linspace(a,b);
-    x = r*cos(t) + h;
-    y = r*sin(t) + k;
-    P = plot(x,y,'b','LineWidth', 3);
-    if ~nargout
-        clear P
-    end
-end
+
 
---
+++ Plot.m
@@ -0,0 +1,216 @@
+classdef Plot
+    methods (Static)
+        
+        function Analyze_TwoParts_CutOff = VisualizeSlicesCutOff(Settings, FigData)
+            if Settings.Show_Plots
+                f = figure('visible', 'on');
+            else
+                f = figure('visible', 'off');
+            end
+            imshow(FigData.I)
+            hold on
+            for k = 1:length(FigData.theta_all)
+                pnt = FigData.points(k, :);
+                roi = [pnt; Settings.Interferometry_Center];
+                plot(roi(:,1), roi(:,2), 'Color', 'red', 'LineWidth', 2)
+                plot(pnt(:,1), pnt(:,2), '.', 'Color', 'red' , 'MarkerSize', 20)
+                text(pnt(:,1), pnt(:,2), num2str(k))
+                clear pnt roi
+            end
+            pnt = drawpoint;
+            Analyze_TwoParts_CutOff = round(norm(pnt.Position-Settings.Interferometry_Center));
+            close(f)
+        end % f = VisualizeSlicesCutOff
+        
+        function f = VisualizeSlices(Settings, FigData)
+            if Settings.Plot_VisualizeSlices && (Settings.Save_Figures || Settings.Show_Plots)
+                if Settings.Show_Plots
+                    f = figure('visible', 'on');
+                else
+                    f = figure('visible', 'off');
+                end
+                imshow(FigData.I)
+                hold on
+                for k = 1:length(FigData.theta_all)
+                    pnt = FigData.points(k, :);
+                    roi = [pnt; Settings.Interferometry_Center];
+                    plot(roi(:,1), roi(:,2), 'Color', 'red', 'LineWidth', 2)
+                    plot(pnt(:,1), pnt(:,2), '.', 'Color', 'red' , 'MarkerSize', 20)
+                    text(pnt(:,1), pnt(:,2), num2str(k))
+                    clear pnt roi
+                end
+                if Settings.Analyze_TwoParts
+                    if Settings.AnalyzeSector
+                        plot_arc(Settings.SectorStart, Settings.SectorEnd, Settings.Interferometry_Center(1), Settings.Interferometry_Center(2), Settings.Analyze_TwoParts_CutOff)
+                    else 
+                        plot_arc(0, 2*pi, Settings.Interferometry_Center(1), Settings.Interferometry_Center(2), Settings.Analyze_TwoParts_CutOff)
+                    end
+                end
+            else
+                f = [];
+            end
+        end % f = VisualizeSlices
+        
+        function f = SingleSliceAnalysis(Settings, FigData)
+            if Settings.Plot_SingleSlice && (Settings.Save_Figures || Settings.Show_Plots)
+                if Settings.Show_Plots
+                    f = figure('visible', 'on');
+                else
+                    f = figure('visible', 'off');
+                end
+                tiledlayout(3,1,'TileSpacing','tight');
+        
+                c_l = 1:length(FigData.c_or);
+                nexttile;
+                plot(c_l, FigData.c_or, 'Color', 'red', 'LineWidth', 2)
+                xlabel('Distance from center [pix]')
+                ylabel('Intensity [arb. units]')
+                xlim([0, max(c_l)])
+                title(strcat('Original slice #', num2str(FigData.slicenumber)))
+
+                nexttile;
+                line(c_l, FigData.c_nor, 'Color', 'red', 'LineWidth', 2)
+                hold on
+                plot(c_l(FigData.pks_locs), FigData.pks, '.', 'MarkerSize', 10, 'Color', 'black')
+                plot(c_l(FigData.mns_locs), -FigData.mns, '.', 'MarkerSize', 10, 'Color', 'green')
+                xlabel('Distance from center [pix]')
+                ylabel('Intensity [arb. units]')
+                xlim([0, max(c_l)])
+                title('After img adjustments and normalization')
+
+                nexttile;
+                plot(c_l, FigData.d_final.*1e6, 'Color', 'blue', 'LineWidth', 2)
+                xlabel('Distance from center [pix]')
+                ylabel('Height [um]')
+                xlim([0, max(c_l)])
+                title('Height profile after model fit')
+            else
+                f = [];
+            end
+        end % f = SingleSliceAnalysis
+        
+        function f = Surface(Settings, FigData)
+            if Settings.Plot_Surface && (Settings.Save_Figures || Settings.Show_Plots)
+                if Settings.Show_Plots
+                    f = figure('visible', 'on');
+                else
+                    f = figure('visible', 'off');
+                end
+                [newXY,~,locMembers] = unique([FigData.data_all(:,1), FigData.data_all(:,2)], 'rows', 'stable');  % if slices overlap, filter out doubles
+                xx = newXY(:,1)';
+                yy = newXY(:,2)';
+                zz = splitapply(@(v) mean(v), FigData.data_all(:,3)', locMembers');  % group the values of z by the duplicate pairs, take the mean
+                T = delaunay(xx, yy);
+                trisurf(T, yy, xx, -zz+max(zz), 'EdgeColor', 'none')
+                % 3d scatter plot
+                % scatter3(data_all(:,1), data_all(:,2), data_all(:,3), [], data_all(:,3), 'filled')
+            else
+                f = [];
+            end
+        end % f = Surface
+        
+        function f = Contour(Settings, FigData)
+            if Settings.Plot_Contour && (Settings.Save_Figures || Settings.Show_Plots)
+                if Settings.Show_Plots
+                    f = figure('visible', 'on');
+                else
+                    f = figure('visible', 'off');
+                end
+                
+                if Settings.Plot_Contour_OverlayOnImage
+                    imshow(FigData.I_or)
+                    hold on
+                end
+                % tri surface
+                x = FigData.data_all(:,1);
+                y = FigData.data_all(:,2);
+                z = FigData.data_all(:,3);
+                [xq,yq] = meshgrid(linspace(min(x),max(x),sqrt(length(x))), ...
+                    linspace(min(y),max(y),sqrt(length(y))));
+                warning('off', 'all')  % TODO: remove duplicate x,y datapoints (average z). Now this throws a warning by griddate (and it is done automatically).
+                    vq = griddata(x,y,z,xq,yq);
+                warning('on', 'all')
+                % s = surf(yq,xq,-vq+max(max(vq)),'EdgeColor','none', 'FaceLighting','gouraud');
+                linetype = '-';  % '-' ':' ':' '-,'  or add colors: '--g'
+                [~, hContour] = contourf(xq,yq,vq, Settings.Plot_Contour_Levels, linetype);
+                hContour.LineWidth = .2;
+                hContour.LineColor = [.5 .5 .5];
+                if Settings.Plot_Contour_OverlayOnImage
+                    drawnow;
+                    hFills = hContour.FacePrims;  % array of TriangleStrip objects
+                    [hFills.ColorType] = deal('truecoloralpha');  % default = 'truecolor'
+                    for idx = 1 : numel(hFills)
+                        hFills(idx).ColorData(4) = Settings.Plot_Contour_Transparency * 255;   % default=255
+                    end
+                end
+                set(gca, 'YDir','reverse')
+                ylim([0, size(FigData.I_or, 1)])
+                xlim([0, size(FigData.I_or, 2)])
+                colorbar;
+            else
+                f = [];
+            end
+        end % f = Contour
+        
+        function f = AverageHeight(Settings, FigData)
+            if Settings.Plot_AverageHeight && (Settings.Save_Figures || Settings.Show_Plots)
+                if Settings.Show_Plots
+                    f = figure('visible', 'on');
+                else
+                    f = figure('visible', 'off');
+                end
+                
+                data_cell = {FigData.data_cell};
+                data_cell_noempties = data_cell(cellfun(@(x) ~isempty(x), data_cell));
+                A = cellfun(@(x) x(:,3), data_cell_noempties, 'UniformOutput', false);
+                array_sizes = cellfun(@(x) length(x(find(~isnan(x),1):end)), A); %exclude leading nans
+                shortest_array = min(array_sizes(array_sizes ~= 0));
+
+                B = nan(length(A), shortest_array);
+                for i = 1:length(A)
+                    if max(~isnan(A{i}))
+                        first_nonnan = find(~isnan(A{i}),1);
+                        data = A{i}(first_nonnan:end);
+                        B(i, :) = data(end-shortest_array+1:end);
+                    end
+                end
+                mean_array = mean(B, 'omitnan');
+                plot(mean_array, 'LineWidth', 3)
+                xlabel('Distance from center[pix]')
+                ylabel('Height [um]')
+            else
+                f = [];
+            end
+        end % f = AverageHeight
+        
+        
+%         function f = AverageHeight(Settings, FigData)
+%             if Settings.Plot_AverageHeight && (Settings.Save_Figures || Settings.Show_Plots)
+%                 if Settings.Show_Plots
+%                     f = figure('visible', 'on');
+%                 else
+%                     f = figure('visible', 'off');
+%                 end
+%             else
+%                 f = [];
+%             end
+%         end % f = AverageHeight
+        
+        
+    end
+end
+
+function P = plot_arc(a,b,h,k,r)
+    % Plot a circular arc as a pie wedge.
+    % a is start of arc in radians, 
+    % b is end of arc in radians, 
+    % (h,k) is the center of the circle.
+    % r is the radius.
+    t = linspace(a,b);
+    x = r*cos(t) + h;
+    y = r*sin(t) + k;
+    P = plot(x,y,'b','LineWidth', 3);
+    if ~nargout
+        clear P
+    end
+end
